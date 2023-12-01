5db6b8af0d0b845a9eb2bc503e6f274dc4b3f3f2
Viper <106883384+Rudal-XD@users.noreply.github.com>
2023-12-01 14:55:46 +0700

    Update run.py

--- run.py
+++ run.py
@@ -153,7 +153,7 @@ for jiah in range(1000):
     l='Mobile Safari/537.36 FBMF/HUAWEI;FBBD/HUAWEI;FBPN/com.facebook.services;com.facebook.orca;com.facebook.lite;com.facebook.katana FBDV/EVR-L29;FBSV/10;FBLR/0;FBBK/1;FBCA/arm64-v8a:;]'
     uaku2=f'{aa} {b}; {c}{d}{e}{f}) {g}{h}.{i}.{j}.{k} {l}'
 #	uaku2=f'{aa} {b}; {c}{e}) {g}{h}.{i}.{j}.{k} {l}'
-    ugen.append(uaku2)
+    free.append(uaku2)
     
 #------------[ UBAH UA DIH SINI OM ]---------------#
 for t in range(10000):
@@ -184,7 +184,7 @@ for xd in range(10000):
     h=random.randrange(40,150)
     i='Mobile Safari/537.36'
     uaku=f'{a} {b}; {c} {buil} {d}{kk}.{g}.{h} {e} {i}'
-    ugen.append(uaku)
+    free.append(uaku)
 
 def uaku():
     try:
@@ -842,7 +842,7 @@ def grup1(urls):
 #------------------[ DUMP-ID ]-----------------#
 def publikv2():
     with requests.Session() as ses:
-        token = open('.token.txt','r').read()
+        token = open('.token1.txt','r').read()
         cok = open('.cok.txt','r').read()
         a = console.input(f" {H2}• {P2}Masukan Id Target :{U2} ")
         filetex = console.input(f" {H2}• {P2}Nama File Dump  :{U2} ")
@@ -869,9 +869,9 @@ def publikv2():
 #-------------------[ CRACK-PUBLIK ]----------------#
 def publik():
     with requests.Session() as ses:
-        token = open('.token.txt','r').read()
+        token = open('.token1.txt','r').read()
         cok = open('.cok.txt','r').read()	
-        toke = open('.token1.txt','r').read()
+        toke = open('.token.txt','r').read()
         prints(Panel(f"""{P2}masukan id target, pastikan id target bersifat publik dan tidak private""",subtitle=f"{P2}ketik {H2}me{P2} untuk dump dari teman sendiri",width=60,style=f"{color_panel}"))
         a = console.input(f" {H2}• {P2}Masukan Id Target :{U2} ")
         if a in ['me','Me','ME']:
