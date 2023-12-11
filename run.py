d0d4814400230f2a6131f1c3715425d36dc9f4ce
Viper <106883384+Rudal-XD@users.noreply.github.com>
2023-12-11 22:03:28 +0700

    Update run.py

--- run.py
+++ run.py
@@ -353,7 +353,7 @@ def login():
         login123()
 
 def login_lagi334():
-    cookie = console.input(f" {H2}• {P2}cookie : ")
+    cookie = Console().input(f" {H2}• {P2}cookie : ")
     try:
         ses.headers.update({"Accept-Language": "id,en;q=0.9","User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36","Referer": "https://www.instagram.com/","Host": "www.facebook.com","Sec-Fetch-Mode": "cors","Accept": "*/*","Connection": "keep-alive","Sec-Fetch-Site": "cross-site","Sec-Fetch-Dest": "empty","Origin": "https://www.instagram.com","Accept-Encoding": "gzip, deflate"})
         link = ses.get("https://www.facebook.com/x/oauth/status?client_id=124024574287414&wants_cookie_data=true&origin=1&input_token=&sdk=joey&redirect_uri=https://www.instagram.com/brutalid_/", cookies={"cookie": cookie})
@@ -362,8 +362,7 @@ def login_lagi334():
             open('.cok.txt','w').write(cookie);open('.token.txt','w').write(token)
             Console().print(Panel(f""" [bold green]{token}""",width=60, style=f"{color_panel}", title="[bold green]TOKEN"))
             follow_me(cookie)
-            Console().print(f" {H2}• {P2}[bold green]Login Berhasil,Sedang Menjalankan Ulang[bold white]")
-            back()
+            Console().print(f" {H2}• {P2}[bold green]Login Berhasil,jalankan Ulang Script[bold white]")
         else:exit()
     except Exception as e:
         Console().print(f" {H2}• {P2}Cookies Invalid")
