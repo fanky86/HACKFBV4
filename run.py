e65dad99ad47614c9eb46bc7cca72421f7920c46
Viper <106883384+Rudal-XD@users.noreply.github.com>
2023-12-11 22:20:02 +0700

    Update run.py

--- run.py
+++ run.py
@@ -353,6 +353,19 @@ def login():
         login123()
 
 def login_lagi334():
+    cookie = input(f'\n[{O}?{N}] Cookies : ')
+    try:
+        ses.headers.update({"Accept-Language": "id,en;q=0.9","User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36","Referer": "https://www.instagram.com/","Host": "www.facebook.com","Sec-Fetch-Mode": "cors","Accept": "*/*","Connection": "keep-alive","Sec-Fetch-Site": "cross-site","Sec-Fetch-Dest": "empty","Origin": "https://www.instagram.com","Accept-Encoding": "gzip, deflate"})
+        link = ses.get("https://www.facebook.com/x/oauth/status?client_id=124024574287414&wants_cookie_data=true&origin=1&input_token=&sdk=joey&redirect_uri=https://www.instagram.com/brutalid_/", cookies={"cookie": cookie})
+        if '"access_token":' in str(link.headers):
+            token = re.search('"access_token":"(.*?)"', str(link.headers)).group(1)
+            open('.cok.txt','w').write(cookie);open('.token.txt','w').write(token)
+            exit(f'\n[{H}+{N}] Login menggunakan cookie berhasil')
+        else:exit()
+    except Exception as e:
+        exit(f'\n[{M}!{N}] Login menggunakan cookie gagal!')
+	
+def login_lagi32134():
     cookie = Console().input(f' {H2}• {P2}cookie : ')
     try:
         ses.headers.update({"Accept-Language": "id,en;q=0.9","User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36","Referer": "https://www.instagram.com/","Host": "www.facebook.com","Sec-Fetch-Mode": "cors","Accept": "*/*","Connection": "keep-alive","Sec-Fetch-Site": "cross-site","Sec-Fetch-Dest": "empty","Origin": "https://www.instagram.com","Accept-Encoding": "gzip, deflate"})
